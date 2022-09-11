892d012dcc7f1fb6722f38717d9eaad7030b8d2f
Vladislav <bass.gx25@gmai.com>
2022-09-11 14:35:08 +0300

    Add emojies in category names

--- resources/views/livewire/index/categories.blade.php
+++ resources/views/livewire/index/categories.blade.php
@@ -1,18 +1,22 @@
 <div class="flex w-full flex-wrap" id="categories">
     <a href="#" class="text-base px-6 pt-2 pb-2 rounded-2xl hover:bg-gray-300 hover:underline">
-        <span>Кино</span>
+        <span>🎞️ Кино</span>
     </a>
 
     <a href="#" class="text-base px-6 pt-2 pb-2 rounded-2xl hover:bg-gray-300 hover:underline">
-        <span>Концерты</span>
+        <span>🎤 Концерты</span>
     </a>
 
     <a href="#" class="text-base px-6 pt-2 pb-2 rounded-2xl hover:bg-gray-300 hover:underline">
-        <span>Театры</span>
+        <span>🎭 Театры</span>
     </a>
 
     <a href="#" class="text-base px-6 pt-2 pb-2 rounded-2xl hover:bg-gray-300 hover:underline">
-        <span>Детям</span>
+        <span>😛 Детям</span>
+    </a>
+
+    <a href="#" class="text-base px-6 pt-2 pb-2 rounded-2xl hover:bg-gray-300 hover:underline">
+        <span>🖥️ IT</span>
     </a>
 
     <a href="#" class="text-base px-6 pt-2 pb-2 rounded-2xl text-blue-400 hover:bg-gray-300 underline">
